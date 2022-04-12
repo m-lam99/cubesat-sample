36f420d2ba5a4c837ce8d0f7549938fd8c459dfc
Clan2098 <clan2098@uni.sydney.edu.au>
2022-04-12 11:08:59 +1000

    Adds functions to receive data and test the transceiver

--- ttc/transceiver.py
+++ ttc/transceiver.py
@@ -19,10 +19,13 @@ MODE_READY = 0x02
 MODE_HOST_BAUD_19200 = 0x01
 MODE_RF_BAUD_9600 = 0x02
 
-DEFAULT_HOST_BAUD = 19200
-TIMEOUT = 2
+DEFAULT_HOST_BAUD = 19200  # bps
+DEFAULT_RF_BAUD = 9600  # bps
+TIMEOUT = 2  # s
 PORT = "/dev/ttyS0"
-WRITE_WAIT_TIME = 0.05
+WRITE_WAIT_TIME = 0.05  # s
+RECEIVE_WAIT_TIME = 0.05  # s
+FIRMWARE = [0x23, 0x56, 0x31, 0x2E, 0x30, 0x31]
 
 ser = serial.Serial(
     PORT,
@@ -46,6 +49,25 @@ def send_command(data: list):
         print(f"Data ({data}) did not send")
 
 
+def receive_data() -> list:
+    """Receive data from the transceiver
+
+    Returns:
+        Data from the transceiver
+    """
+    while True:
+        received_data: list = ser.read()
+        sleep(RECEIVE_WAIT_TIME)
+        if len(received_data) == 0:
+            print("Timeout!")
+        else:
+            break
+
+    data_left = ser.inWaiting()
+    received_data += ser.read(data_left)
+    return received_data
+
+
 def setup_transceiver(error_checking: bool = False):
     """Setup the transceiver (ground station)
 
@@ -64,3 +86,20 @@ def setup_transceiver(error_checking: bool = False):
 
     if error_checking:
         send_command(CMD_DATA_ERROR_CHECKING + [0x01])
+
+
+def test_transceiver() -> bool:
+    """Tests the transciever setup by checking the firmware verison
+
+    Returns:
+        Boolean to verify whether the transceiver is working or not
+    """
+    result = False
+
+    send_command(CMD_FIRMWARE_VERSION)
+    firmware_version = receive_data()
+
+    if firmware_version == FIRMWARE:
+        result = True
+
+    return result
