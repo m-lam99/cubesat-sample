565f544fa6c5e782044b695dc37e31002c818737
Connor Langford <clan2098@uni.sydney.edu.au>
2022-04-23 16:20:59 +1000

    Adds main function for testing transceiver functions

--- ttc/serial_test.py
+++ ttc/serial_test.py
@@ -1,37 +1,14 @@
-import serial
-from time import sleep
+# import serial
+# from time import sleep
 
-ser = serial.Serial("/dev/ttyS0", baudrate=19200, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, timeout=5)
-print("Sending Message")
+from transceiver import setup_transceiver, test_transceiver
 
-if not ser.is_open:
-    ser.open()
 
-#ser.write([0x41,0x54,0x4d,0x01])
-ser.write([0x41,0x54,0x3F])
-print("Message Sent!")
-received_data = ser.read()
-if received_data:
-    print("Message Received!")
-else:
-    print("Timeout!")
-sleep(0.3)
-data_left = ser.inWaiting()
-received_data += ser.read(data_left)
-print(received_data)
-sleep(3)
+def main():
 
-print("Sending Message")
-ser.write([0x41,0x54,0x3F])
-print("Message Sent!")
-received_data = ser.read()
-if received_data:
-    print("Message Received!")
-else:
-    print("Timeout!")
-sleep(0.3)
-data_left = ser.inWaiting()
-print(data_left)
-received_data += ser.read(data_left)
-print(received_data)
-sleep(3)
+    setup_transceiver()
+    test_transceiver()
+
+
+if __name__ == "__main__":
+    main()
