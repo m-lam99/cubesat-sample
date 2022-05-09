c079abd1def6d92a811c89e3ccc1fb091b5ad69d
Adam-Hammo <adamhammo99@gmail.com>
2022-05-09 20:49:01 +1000

    Added some structure to ground station code

---
+++ ttc/groundstation/db/db.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+DB_HOST = "ground-station.db"
+
+class DB:
+
+    def __init__(self):
+        self.con = sqlite3.connect(DB_HOST)
+        
+        self.initialise()
+        
+    def initialise(self):
+        with self.con.cursor() as cursor:
+            cursor.execute("""
+                CREATE TABLE IF NOT EXISTS wod
+                (TODO)
+                        """)
--- ttc/groundstation/main.py
+++ ttc/groundstation/main.py
@@ -1,1 +1,27 @@
-# TODO: everything
+import sys
+import transceiver
+
+HELP_MSG = """
+Usage: python3 main.py [live|demo|import]
+                        live: listen to transceiver
+                        demo: use sample/stored data
+                        import: test data import feature
+"""
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print(HELP_MSG)
+        sys.exit(1)
+    
+    
+    
+    if sys.argv[1] == "import":
+        transceiver.test_data_pipeline.run()
+    elif sys.argv[1] == "live":
+        # TODO: launch transceiver listening stuff on a different thread
+        raise NotImplementedError("live integration not setup yet")
+    elif sys.argv[1] == "demo":
+        pass
+    else:
+        print(HELP_MSG)
+        sys.exit(1)
