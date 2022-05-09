e8bc468454b8119862d7ac7b920af47c5a411f5b
Connor Langford <clan2098@uni.sydney.edu.au>
2022-05-09 14:50:27 +1000

    Adds CPP UART fucntionality

--- ttc/groundstation/serial_test.py
+++ ttc/groundstation/serial_test.py
@@ -1,6 +1,12 @@
 from time import sleep
 
-from transceiver import setup_transceiver, test_transceiver, receive_data
+from transceiver import (
+    setup_transceiver,
+    test_transceiver,
+    receive_data,
+    send_command,
+    transmit_message,
+)
 
 
 def main():
@@ -8,11 +14,19 @@ def main():
     while True:
         setup_transceiver(True)
         if test_transceiver():
-            # message = "General Kenobi!".encode()
-            # test_message = [int(hex(x), 16) for x in message]
+            message = "General Kenobi!".encode()
+            test_message = [int(hex(x), 16) for x in message]
             print(receive_data(packet_size=15))
-            # transmit_message(test_message)
+            transmit_message(test_message)
             sleep(5)
+
+    # while True:
+    #     send_command([0x41, 0x54, 0x56])
+    #     print("Sent!")
+    #     sleep(2)
+    # while True:
+    #     received_data: list = _ser.read()
+    #     print(received_data)
 
 
 if __name__ == "__main__":
