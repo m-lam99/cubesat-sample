630ebe1c5ac0580d976bc89a24154bdb63573918
maddie <madelinelam99@gmail.com>
2022-05-16 11:52:41 +1000

    encode current sensor values from float to int

--- obc/include/WholeOrbit.h
+++ obc/include/WholeOrbit.h
@@ -4,27 +4,29 @@
 #define WHOLE_ORBIT_H
 
 #include <stdint.h>
+
 #include "INA219.h"
 
 class WholeOrbit {
+   public:
+    WholeOrbit();
+    void GetData();
+    ~WholeOrbit();
+
    private:
     uint32_t time_;
     bool mode_;
-    uint8_t voltage_batt_;
-    uint8_t current_batt_;
-    uint8_t current_3v3_;
-    uint8_t current_5v_;
-    uint8_t temp_comms_;
-    uint8_t temp_eps_;
-    uint8_t temp_batt_;
+    uint8_t voltage_batt_ = 0;
+    uint8_t current_batt_ = 0;
+    uint8_t current_3v3_ = 0;
+    uint8_t current_5v_ = 0;
+    uint8_t temp_comms_ = 0;
+    uint8_t temp_eps_ = 0;
+    uint8_t temp_batt_ = 0;
 
     INA219 current_sensor_batt_;
     INA219 current_sensor_3v3_;
     INA219 current_sensor_5v_;
-
-   public:
-    WholeOrbit();
-    ~WholeOrbit();
 };
 
 #endif
--- obc/src/WholeOrbit.cpp
+++ obc/src/WholeOrbit.cpp
@@ -1,10 +1,26 @@
 #include "WholeOrbit.h"
+#include <algorithm>
+#include <cmath>
 
 
 WholeOrbit::WholeOrbit(): 
     current_sensor_batt_(1, INA219_ADDRESS_BATT),
     current_sensor_3v3_(1, INA219_ADDRESS_3V3),
     current_sensor_5v_(1, INA219_ADDRESS_5V) {
+}
+
+void WholeOrbit::GetData(){
+    // IUI8 = min(0, max(28-1, floor( 127 * I ) + 127) )
+    float current_batt_f = current_sensor_batt_.current();
+    current_batt_ = std::min(0, std::max(1<<8 - 1, (int)floor(127*current_batt_f)+127));
+
+    // IUI8 = min(0, max(28-1, floor( 40 * I ) ) )
+    float current_3v3_f = current_sensor_3v3_.current();
+    current_3v3_ = std::min(0, std::max(1<<8 -1, (int)floor(40*current_3v3_f)));
+
+    float current_5v_f = current_sensor_5v_.current();
+    current_5v_ = std::min(0, std::max(1<<8 -1, (int)floor(40*current_5v_f)));
+
 }
 
 WholeOrbit::~WholeOrbit()
