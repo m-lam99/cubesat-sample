31044be746dd0814ee5716d3ff2db5d92c9d5f8d
Clan2098 <clan2098@uni.sydney.edu.au>
2022-05-16 15:35:04 +1000

    Adds transmit message function

--- ttc/sat_telem/include/transceiver.h
+++ ttc/sat_telem/include/transceiver.h
@@ -5,24 +5,27 @@
 #include <termios.h>
 #include <unistd.h>
 #include <array>
+#include <vector>
+#include <stdint.h>
 
 class Transceiver
 {
 
 public:
     static const int MAX_BYTES_AX25 = 42;
+    static const int CMD_MAX_LENGTH = 47;
 
     Transceiver();
     ~Transceiver();
-    void SendCommand(const char *data);
-    std::array<char, MAX_BYTES_AX25> ReceiveData(std::array<char, MAX_BYTES_AX25> buff);
+    void SendCommand(std::vector<uint8_t> data);
+    std::vector<uint8_t> ReceiveData(void);
     bool TestTransceiver(void);
-    void TransmitMessage(void);
+    void TransmitMessage(std::vector<uint8_t> message);
 
 private:
     const char CMD_OPERATING_MODE[3] = {0x41, 0x54, 0x4D};
     const char CMD_RECEIVE_MODE_CONFIG[3] = {0x41, 0x54, 0x52};
-    const char CMD_TRANSMIT_MODE_CONFIG[3] = {0x41, 0x54, 0x53};
+    std::vector<uint8_t> CMD_TRANSMIT_MODE_CONFIG = {0x41, 0x54, 0x53, 0, 47};
     const char CMD_SYNC_BYTES[3] = {0x41, 0x54, 0x41};
     const char CMD_HOST_BAUD_RATE[3] = {0x41, 0x54, 0x48};
     const char CMD_RF_BAUD_RATE[3] = {0x41, 0x54, 0x42};
@@ -31,7 +34,7 @@ private:
     const char CMD_ENABLE_RESPONSE[3] = {0x41, 0x54, 0x43};
     const char CMD_RSSI[3] = {0x41, 0x54, 0x51};
     const char CMD_CONFIGURATION_SETTINGS[3] = {0x41, 0x54, 0x3F};
-    const char CMD_FIRMWARE_VERSION[3] = {0x41, 0x54, 0x56};
+    std::vector<uint8_t> CMD_FIRMWARE_VERSION = {0x41, 0x54, 0x56};
     const char CMD_RESET_DEFAULT[3] = {0x41, 0x54, 0x44};
     const char MODE_READY = 0x02;
     const char MODE_HOST_BAUD_19200 = 0x01;
