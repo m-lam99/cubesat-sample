768447bddbcc7d4e97e03dcbf6620e0edbd5fed9
Ricky Schlessinger <rsch5781@uni.sydney.edu.au>
2022-05-19 16:44:26 +1000

    read config

--- obc/src/ADS1015.cpp
+++ obc/src/ADS1015.cpp
@@ -58,7 +58,7 @@ float ADS1015::getVoltage(int channel) {
             perror("No >:(");
             break;
     }
-    std::cout << "config" << readRegisters(ADDRESS_POINTER::CONFIG) << std::endl;
+    std::cout << "config " << config || (0b100 << 12) << std::endl;
     uint16_t bit_val = readRegisters(ADDRESS_POINTER::CONVERSION);
     std::cout << "bit " << bit_val << std::endl;
     float voltage;
