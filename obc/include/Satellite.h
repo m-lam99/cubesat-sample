1223f71600caa0d5896267c071af88dc121b6d52
maddie <madelinelam99@gmail.com>
2022-05-22 14:17:28 +1000

    added sensors

--- obc/include/Satellite.h
+++ obc/include/Satellite.h
@@ -13,8 +13,6 @@
 #include "WholeOrbit.h"
 
 class Satellite {
-   private:
-    
    public:
     Satellite(/* args */);
     int detumbling();
@@ -28,6 +26,16 @@ class Satellite {
     int checkTransceiver();
     int initialize();
     ~Satellite();
+
+   private:
+    ADS1015 adc1_;
+    ADS1015 adc2_;
+    AS7263 payload_;
+    BNO055 imu_;
+    // GPS
+    WholeOrbit wod_;
+    // PWM
+
 };
 
 #endif
