ed91e7e93b4488b66d063419695efcb3b389ba19
Demo User <debian@beaglebone.localdomain>
2022-05-22 10:30:29 +0000

    bb

--- obc/obj/main.d
+++ obc/obj/main.d
@@ -1,8 +1,8 @@
 obj/main.o: src/main.cpp include/ADS1015.h include/I2C.h include/GPIO.h \
  include/I2C.h include/INA219.h include/GPS.h include/UART.h \
- include/BNO055.h include/Sensor.h include/utility/imumaths.h \
- include/utility/vector.h include/utility/matrix.h \
- include/utility/quaternion.h include/PWM.h
+ include/AS7263.h include/BNO055.h include/Sensor.h \
+ include/utility/imumaths.h include/utility/vector.h \
+ include/utility/matrix.h include/utility/quaternion.h include/PWM.h
 
 include/ADS1015.h:
 
@@ -17,6 +17,8 @@ include/INA219.h:
 include/GPS.h:
 
 include/UART.h:
+
+include/AS7263.h:
 
 include/BNO055.h:
 
