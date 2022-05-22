b50098f243a7dd527edd8c941a23b1caed4043f2
Demo User <debian@beaglebone.localdomain>
2022-05-22 04:55:02 +0000

    changes

--- obc/obj/main.d
+++ obc/obj/main.d
@@ -1,5 +1,8 @@
 obj/main.o: src/main.cpp include/ADS1015.h include/I2C.h include/GPIO.h \
- include/I2C.h include/INA219.h
+ include/I2C.h include/INA219.h include/GPS.h include/UART.h \
+ include/BNO055.h include/Sensor.h include/utility/imumaths.h \
+ include/utility/vector.h include/utility/matrix.h \
+ include/utility/quaternion.h
 
 include/ADS1015.h:
 
@@ -10,3 +13,19 @@ include/GPIO.h:
 include/I2C.h:
 
 include/INA219.h:
+
+include/GPS.h:
+
+include/UART.h:
+
+include/BNO055.h:
+
+include/Sensor.h:
+
+include/utility/imumaths.h:
+
+include/utility/vector.h:
+
+include/utility/matrix.h:
+
+include/utility/quaternion.h:
