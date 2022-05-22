949d9b2b42af42c29f9aa4332a39cd65c14c5803
Yong Li <yongli@Yongs-MacBook-Pro.local>
2022-05-22 20:24:51 +1000

    PWM

--- obc/src/main.cpp
+++ obc/src/main.cpp
@@ -99,9 +99,9 @@ void testAS7263() {
 void testPWM(){
    // 
     PWM pwm(PWM_0B);  // P9_42 MUST be loaded as a slot before use
-    pwm.setPeriod(100000);         // Set the period in ns
+    pwm.setPeriod(1000000000);         // Set the period in ns
     std::cout << "set period" << std::endl; 
-     pwm.setDutyCycle(100.0f);       // Set the duty cycle as a percentage
+     pwm.setDutyCycle(50.0f);       // Set the duty cycle as a percentage
     std::cout << "set duty" << std::endl; 
 
     pwm.setPolarity(PWM::ACTIVE_LOW);  // using active low PWM
