2cc060549353bafcbc7cd75ab0845763fa6c1085
Yong Li <yongli@Yongs-MacBook-Pro.local>
2022-05-23 16:11:14 +1000

    Change ttc parity bits

--- obc/src/UART.cpp
+++ obc/src/UART.cpp
@@ -93,6 +93,26 @@ int UARTDevice::config(){
     tty.c_oflag = 0;
     tty.c_lflag = 0;
 
+    // FOR TTC 
+    tty.c_cflag &= ~PARENB;
+    tty.c_cflag &= ~CSTOPB;
+    tty.c_cflag &= ~CSIZE;
+    tty.c_cflag |= CS8;
+    tty.c_cflag &= ~CRTSCTS;
+    tty.c_cflag |= CREAD | CLOCAL;
+    tty.c_lflag &= ~ICANON;
+    tty.c_lflag &= ~ECHO;
+    tty.c_lflag &= ~ECHOE;
+    tty.c_lflag &= ~ECHONL;
+    tty.c_lflag &= ~ISIG;
+    tty.c_iflag &= ~(IXON | IXOFF | IXANY);
+    tty.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL);
+    tty.c_oflag &= ~OPOST;
+    tty.c_oflag &= ~ONLCR;
+    tty.c_cc[VTIME] = 10;
+    tty.c_cc[VMIN] = 0;
+
+
     tcflush(serial_port, TCIFLUSH);
     tcsetattr(serial_port, TCSANOW, &tty);
 
