0672f453049540a15cbceb610fb21e7b0810611f
Yong Li <yongli@Yongs-MacBook-Pro.local>
2022-05-23 17:28:29 +1000

    change settings

--- obc/include/transceiver.h
+++ obc/include/transceiver.h
@@ -29,7 +29,7 @@ public:
 private:
     const char CMD_OPERATING_MODE[3] = {0x41, 0x54, 0x4D};
     const char CMD_RECEIVE_MODE_CONFIG[3] = {0x41, 0x54, 0x52};
-    std::vector<uint8_t> CMD_TRANSMIT_MODE_CONFIG = {0x41, 0x54, 0x53, 0, MAX_BYTES_AX25};
+    std::vector<uint8_t> CMD_TRANSMIT_MODE_CONFIG = {0x41, 0x54, 0x53, 0, 6};
     const char CMD_SYNC_BYTES[3] = {0x41, 0x54, 0x41};
     const char CMD_HOST_BAUD_RATE[3] = {0x41, 0x54, 0x48};
     const char CMD_RF_BAUD_RATE[3] = {0x41, 0x54, 0x42};
