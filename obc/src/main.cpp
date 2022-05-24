79de963a60458348d48b822f42e4ef9d401628b6
Yong Li <yongli@Yongs-MacBook-Pro.local>
2022-05-24 17:46:36 +1000

    test

--- obc/src/main.cpp
+++ obc/src/main.cpp
@@ -133,7 +133,7 @@ void testAS7263()
 void testPWM(string pwm_channel){
    // 
     PWM pwm(pwm_channel);  // P9_42 MUST be loaded as a slot before use
-    pwm.setPeriod(30503280);         // Set the period in ns
+    pwm.setPeriod(21234567);         // Set the period in ns
     pwm.setDutyCycle(50.0f);       // Set the duty cycle as a percentage
 
     pwm.setPolarity(PWM::ACTIVE_LOW);  // using active low PWM
