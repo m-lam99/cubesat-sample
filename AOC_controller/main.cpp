3a8b9e59dd639ee24ce50f63d540a70e6fb09183
Zainab Al Nakeeb <zainab.alnakeeb@hotmail.com>
2022-05-25 12:06:17 +1000

    p2 controller, needs adjustments in 3 locations for integration
    code currently uses dummy values where IMU data is required

---
+++ AOC_controller/main.cpp
@@ -0,0 +1,27 @@
+// ----- Main file ------ //
+
+// This algorithm aims to run a double proportional control algorithm in quaternion space. Wish us luck.
+
+// POINTS FOR INTEGRATION:
+// - need three integration points
+// - will be fed target pointing position in euler angles (main.cpp)
+// - will need constant readings from IMU (both in controller.cpp) for 
+//  current position in quaternions and current angular velocity
+// - code also includes several 'print to terminal' checks that can be removed
+
+#include "quaternion.h"
+#include"controller.h"
+#include <iostream>
+
+int main(void)
+{
+
+    // we receive a target orientation in euler angles
+    double references[3] = {0.3, 1.2, 0.75};
+
+    // create controller object and run algorithm
+    CControl newController(references);
+    newController.runControlAlgorithm();
+
+    return 0;
+}
