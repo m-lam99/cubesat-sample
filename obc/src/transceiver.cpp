cd1d6ffa8be1934713da9ba2ccd3ff174a324a91
Ricky Schlessinger <rsch5781@uni.sydney.edu.au>
2022-05-31 12:33:57 +1000

    ttc

--- obc/src/transceiver.cpp
+++ obc/src/transceiver.cpp
@@ -84,6 +84,7 @@ std::vector<uint8_t> Transceiver::ReceiveData(void)
 
 bool Transceiver::TestTransceiver(void)
 {
+    cout << "help? " << endl;
     SendCommand(CMD_FIRMWARE_VERSION);
     std::vector<uint8_t> buff;
 
